efd22110fd5a811250e3c2959e578e7fa0a6a5eb
Jan Haans <jan.haans@gmail.com>
2016-03-31 21:38:47 +0200

    Changed routes and added new controller files

---
+++ app_server/controllers/locations.js
@@ -0,0 +1,11 @@
+module.exports.homelist = function(req, res, next) {
+	res.render('index', { title: 'Home'});
+};
+
+module.exports.locationInfo = function(req, res, next) {
+	res.render('index', { title: 'Location Info'});
+};
+
+module.exports.addReview = function(req, res, next) {
+	res.render('index', { title: 'Add Review'});
+};
